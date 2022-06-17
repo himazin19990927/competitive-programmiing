6da86bfcd083976c32e226968f1166ecb33eb9ce
Taiki Harada <haradataiki19990927@gmail.com>
2022-06-17 02:05:57 +0000

    Add code6_4, code6_7

---
+++ book_algorithm_solution/chapter6/code6_7.cpp
@@ -0,0 +1,57 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+
+    vector<long long> H(N), S(N);
+    for(int i = 0; i < N; i++) {
+        cin >> H[i] >> S[i];
+    }
+
+    // 二分探索の上限値を求める
+    // N秒後の風船の高さの最大値が、ペナルティの最大値となる
+    long long M = 0;
+    for(int i = 0; i < N; i++) {
+        // すべての風船を割り終わるにはN秒かかる
+        M = max(M, H[i] + S[i] * N);
+    }
+
+    // 二分探索
+    long long left = 0, right = M;
+    while(right - left > 1) {
+        long long mid = (left + right) / 2;
+
+        // 判定する
+        bool ok = true;
+        vector<long long> t(N, 0); // 各風船を割るまでの制限時間
+        for(int i = 0; i < N; i++) {
+            // そもそもmidが初期高度より低かったらfalse
+            if(mid < H[i]) {
+                ok = false;
+            } else {
+                t[i] = (mid - H[i]) / S[i];
+            }
+        }
+
+        // 制限時間が差し迫っている順にソート
+        sort(t.begin(), t.end());
+        for(int i = 0; i < N; i++) {
+            // 時間切れ発生の場合はfalse
+            if(t[i] < i) {
+                ok = false;
+            }
+        }
+
+        if(ok) {
+            right = mid;
+        } else {
+            left = mid;
+        }
+    }
+
+    cout << right << endl;
+}
