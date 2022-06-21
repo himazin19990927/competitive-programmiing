c4400dc1764b8a6c1dd64cc8dd5bfea9ae799d3e
Taiki Harada <haradataiki19990927@gmail.com>
2022-06-21 14:15:52 +0000

    Add constant values to templete file

--- atcoder/(templete)/main.cpp
+++ atcoder/(templete)/main.cpp
@@ -1,6 +1,9 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-    
-}
+const int IINF = 0x3f3f3f3f;
+const long long LINF = 0x3f3f3f3f3f3f3f3fLL;
+constexpr int INF = sizeof(int) == sizeof(long long) ? LINF : IINF;
+const double eps = 1e-15;
+
+int main() {}
